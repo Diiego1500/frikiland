5245f6894ec84bb8d882b9d80f5b0cf4dadaf7db
Diego Bastidas <diegofernando150@gmail.com>
2022-11-08 20:39:44 -0500

    instalacion liip_imagine bundle, previo al uso

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
